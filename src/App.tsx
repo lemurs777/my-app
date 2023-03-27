28d69973c2dee22dc8137a45fed370c3768ac909
lemurs777 <lemurs.agency@gmail.com>
2023-03-27 19:00:59 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+import Header from './components/header';
 import './scss/style.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello world!</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
     </div>
   );
 }
