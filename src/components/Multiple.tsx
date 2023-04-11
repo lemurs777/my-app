4ffb266415342d5ec88a5211c5ca4cef277a6acf
lemurs777 <lemurs.agency@gmail.com>
2023-04-11 09:28:04 +0300

    update aside

--- src/components/Multiple.tsx
+++ src/components/Multiple.tsx
@@ -15,23 +15,121 @@ function Multiple() {
                             <div className="multiple__item-top-names">Bayern Munich - Eintracht Frankfurt</div>
                             <button className="multiple__item-top-btn  multiple__item-top-btn--close">Close</button>
                         </div>
-                    </li>
+                        <div className="multiple__item-bottom">
+                            <div className="multiple__item-bottom-left">
+                                <div className="multiple__item-bottom-left-title">
+                                    3:2
+                                </div>
+                                <div className="multiple__item-bottom-left-info">
+                                    Exact score
+                                </div>                               
+                            </div>
+                            <div className="multiple__item-bottom-right">
+                                <div className="multiple__item-bottom-right-coefficient">
+                                    3.05
+                                </div>
+                            </div>
+                        </div>
+                    </li> 
                     <li className="multiple__item">
-                    <div className="multiple__item-top">
+                        <div className="multiple__item-top">
                             <button className="multiple__item-top-btn  multiple__item-top-btn--check">Check</button>
                             <div className="multiple__item-top-names">Borussia Dortmund - Schalke</div>
                             <button className="multiple__item-top-btn  multiple__item-top-btn--close">Close</button>
                         </div>
+                        <div className="multiple__item-bottom">
+                            <div className="multiple__item-bottom-left">
+                                <div className="multiple__item-bottom-left-title multiple__item-bottom-left-title--live">
+                                    Сток Сити (-1.0)
+                                </div>
+                                <div className="multiple__item-bottom-left-info">
+                                    1st card: Who will destroy the next.
+                                </div>                          
+                            </div>
+                            <div className="multiple__item-bottom-right">
+                                <div className="multiple__item-bottom-right-coefficient multiple__item-bottom-right-coefficient--upp">
+                                    2.63
+                                </div>
+                                <div className="multiple__item-bottom-right-old-coefficient">
+                                    1122.10
+                                </div>
+                            </div>
+                        </div>
                     </li>
+                    <li className="multiple__item">
+                        <div className="multiple__item-top">
+                            <button className="multiple__item-top-btn  multiple__item-top-btn--check">Check</button>
+                            <div className="multiple__item-top-names">Borussia Dortmund - Schalke</div>
+                            <button className="multiple__item-top-btn  multiple__item-top-btn--close">Close</button>
+                        </div>
+                        <div className="multiple__item-bottom">
+                            <div className="multiple__item-bottom-left">
+                                <div className="multiple__item-bottom-left-title multiple__item-bottom-left-title--live">
+                                    Сток Сити (-1.0)
+                                </div>
+                                <div className="multiple__item-bottom-left-info">
+                                    1st card: Who will destroy the next.
+                                </div>                          
+                            </div>
+                            <div className="multiple__item-bottom-right">
+                                <div className="multiple__item-bottom-right-coefficient multiple__item-bottom-right-coefficient--upp">
+                                    2.63
+                                </div>
+                                <div className="multiple__item-bottom-right-old-coefficient">
+                                    1122.10
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li className="multiple__item">
+                        <div className="multiple__item-top">
+                            <button className="multiple__item-top-btn  multiple__item-top-btn--check">Check</button>
+                            <div className="multiple__item-top-names">Borussia Dortmund - Schalke</div>
+                            <button className="multiple__item-top-btn  multiple__item-top-btn--close">Close</button>
+                        </div>
+                        <div className="multiple__item-bottom">
+                            <div className="multiple__item-bottom-left">
+                                <div className="multiple__item-bottom-left-title multiple__item-bottom-left-title--live">
+                                    Сток Сити (-1.0)
+                                </div>
+                                <div className="multiple__item-bottom-left-info">
+                                    1st card: Who will destroy the next.
+                                </div>                          
+                            </div>
+                            <div className="multiple__item-bottom-right">
+                                <div className="multiple__item-bottom-right-coefficient multiple__item-bottom-right-coefficient--upp">
+                                    992.63
+                                </div>
+                                <div className="multiple__item-bottom-right-old-coefficient">
+                                    1122.10
+                                </div>
+                            </div>
+                        </div>
+                    </li> 
                     <li className="multiple__item">
                         <div className="multiple__item-top">
                             <button className="multiple__item-top-btn multiple__item-top-btn--check multiple__item-top-btn--check-ok">Check</button>
                             <div className="multiple__item-top-names">Stuttgart - Cologne</div>
                             <button className="multiple__item-top-btn  multiple__item-top-btn--close">Close</button>
                         </div>
+                        <div className="multiple__item-bottom">
+                            <div className="multiple__item-bottom-left">
+                                <div className="multiple__item-bottom-left-title">
+                                    X2
+                                </div>
+                                <div className="multiple__item-bottom-left-info">
+                                    Double outcome
+                                </div>                              
+                            </div>
+                            <div className="multiple__item-bottom-right">
+                                <div className="multiple__item-bottom-right-coefficient">
+                                    1.75
+                                </div>
+                            </div>
+                        </div>
                     </li>
                 </ul>
-            </div>
+			</div>
 		</div>
 	);
 }
