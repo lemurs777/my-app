8e71aa6d4ee10480a3376b77ef3c411a86f0c5ae
lemurs777 <lemurs.agency@gmail.com>
2023-04-15 18:24:48 +0300

    update profile page

--- src/components/Faq.tsx
+++ src/components/Faq.tsx
@@ -1,9 +1,14 @@
 function Faq() {
-    return (
-        <div className="faq">
-          
-      </div>
-    );
-  }
-  
-  export default Faq;
+	return (
+	<div className="faq">
+		<h2 className="faq__title">Вопрос – ответ</h2>
+		<ul className="faq__list">
+			<li className="faq__item">Какие доступные форматы коэффициентов?</li>
+			<li className="faq__item">Какие ограничения доступны для аккаунта?</li>
+			<li className="faq__item">В чём разница между одинаром и экспрессом?</li>
+		</ul>
+	</div>
+	);
+}
+
+export default Faq;
