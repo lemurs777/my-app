f2af649322c28fb2b8d3c2878df5c97eb39362da
lemurs777 <lemurs.agency@gmail.com>
2023-04-24 19:30:38 +0300

    update profile page and start work with js

--- src/components/MyProfile.tsx
+++ src/components/MyProfile.tsx
@@ -1,4 +1,101 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import clsx from 'clsx';
+
 function MyProfile() {
+
+	const arrProfileInfo = [
+		{
+			title: 'Персональные данные',
+			info: 'Персональная информация и верификация аккаунта',
+			iconClass: 'profile__header-icon--info',
+			infoItems: [
+				{
+					name: 'Персональная информация',
+					link: '/profile/',
+					isProbem: false,
+				},
+				{
+					name: 'Верификация аккаунта +',
+					link: '/profile/verification',
+					isProbem: true,
+				},
+			],
+		},
+		{
+			title: 'Транзакции',
+			info: 'Пополнение, вывод и история транзакций',
+			iconClass: 'profile__header-icon--transactions',
+			infoItems: [
+				{
+					name: 'Транзакции +',
+					link: '/profile/conclusions',
+					isProbem: false,
+				},
+			],
+		},
+		{
+			title: 'Мои ставки',
+			info: 'Ваши активные и рассчитанные ставки',
+			iconClass: 'profile__header-icon--bets',
+			infoItems: [
+				{
+					name: 'История ставок +',
+					link: '/profile/betting',
+					isProbem: false,
+				},
+			],
+		},
+		{
+			title: 'Мои промокоды',
+			info: 'Промокоды, которые подходят именно вам',
+			iconClass: 'profile__header-icon--promo',
+			infoItems: [
+				{
+					name: 'Мои промокоды +',
+					link: '/profile/promo',
+					isProbem: false,
+				},
+			],
+		},
+		{
+			title: 'Настройки платформы',
+			info: 'Настройка сервиса под себя',
+			iconClass: 'profile__header-icon--settings',
+			infoItems: [
+				{
+					name: 'Наствойки',
+					link: '/profile/',
+					isProbem: false,
+				},
+			],
+		},
+		{
+			title: 'Помощь и информация',
+			info: 'Служба поддержки и полезная информация',
+			iconClass: 'profile__header-icon--support',
+			infoItems: [
+				{
+					name: 'Служба поддержки',
+					link: '/profile/',
+					isProbem: false,
+				},
+				{
+					name: 'Полезная информация',
+					link: '/profile/',
+					isProbem: false,
+				},
+			],
+		},
+	];
+
+	const [isOpen, setIsOpen] = useState(true);
+
+	const handleClick = (event: any) => {
+		setIsOpen(current => !current);
+		console.log(event.currentTarget);
+	}
+
 	return (
 		<div className="profile">
 			<div className="profile__title-wrapper">
@@ -6,66 +103,36 @@ function MyProfile() {
 				<button className="profile__title-btn">More</button>
 			</div>
 			<ul className="profile__list">
-				<li className="profile__item">
-					<div className="profile__header">
-						<div className="profile__header-icon"></div>
-						<h3 className="profile__header-title">Персональные данные</h3>
-						<button className="profile__header-btn-more">More</button>
-					</div>
-					<div className="profile__item-info">
-						Персональная информация и верификация аккаунта
-					</div>
-				</li>
-				<li className="profile__item">
-					<div className="profile__header">
-						<div className="profile__header-icon"></div>
-						<h3 className="profile__header-title">Транзакции</h3>
-						<button className="profile__header-btn-more">More</button>
-					</div>
-					<div className="profile__item-info">
-						Пополнение, вывод и история транзакций
-					</div>
-				</li>
-				<li className="profile__item">
-					<div className="profile__header">
-						<div className="profile__header-icon"></div>
-						<h3 className="profile__header-title">Мои ставки</h3>
-						<button className="profile__header-btn-more">More</button>
-					</div>
-					<div className="profile__item-info">
-						Ваши активные и рассчитанные ставки
-					</div>
-				</li>
-				<li className="profile__item">
-					<div className="profile__header">
-						<div className="profile__header-icon"></div>
-						<h3 className="profile__header-title">Мои промокоды</h3>
-						<button className="profile__header-btn-more">More</button>
-					</div>
-					<div className="profile__item-info">
-						Промокоды, которые подходят именно вам
-					</div>
-				</li>
-				<li className="profile__item">
-					<div className="profile__header">
-						<div className="profile__header-icon"></div>
-						<h3 className="profile__header-title">Настройки платформы</h3>
-						<button className="profile__header-btn-more">More</button>
-					</div>
-					<div className="profile__item-info">
-						Настройка сервиса под себя
-					</div>
-				</li>
-				<li className="profile__item">
-					<div className="profile__header">
-						<div className="profile__header-icon"></div>
-						<h3 className="profile__header-title">Помощь и информация</h3>
-						<button className="profile__header-btn-more">More</button>
-					</div>
-					<div className="profile__item-info">
-						Служба поддержки и полезная информация
-					</div>
-				</li>
+				{arrProfileInfo.map((profileInfo, index) => (
+					<li key={index} className="profile__item">
+						<div className="profile__header">
+							<div className={clsx(
+							'profile__header-icon',
+							profileInfo.iconClass,
+							{'profile__header-icon--open': isOpen}
+						)}></div>
+							<h3 className="profile__header-title">{profileInfo.title}</h3>
+							<button className="profile__header-btn-more" onClick={handleClick}>More</button>
+						</div>
+						<div className={clsx(
+							'profile__item-info',
+							{'profile__item-info--open': !isOpen}
+						)}>{profileInfo.info}</div>
+						<ul className={clsx(
+							'profile__sublist',
+							{'profile__sublist--open': isOpen}
+						)}>
+							{profileInfo.infoItems.map((infoItem, index) => (
+								<li key={index} className="profile__subitem">
+									<Link className={clsx(
+											'profile__link',
+											{'profile__link--problem': infoItem.isProbem}
+										)} to={infoItem.link}>{infoItem.name}</Link>
+								</li>
+							))};
+						</ul>	
+					</li>	
+				))};
 			</ul>
 		</div>
 	);
