d5d547c0b430d80bbaf2c4bb3fee9dca62575a45
lemurs777 <lemurs.agency@gmail.com>
2023-05-31 14:12:28 +0300

    New update page statistic six

--- src/components/StatisticsSix.tsx
+++ src/components/StatisticsSix.tsx
@@ -1,6 +1,10 @@
+import { useState } from 'react';
 import clsx from 'clsx';
 
 function StatisticsSix() {
+
+    const [showTable, setShowTable] = useState(false);
+    const [showTableTwo, setShowTableTwo] = useState(false);
 
 	const arrRowsOne = [
 		{
@@ -290,6 +294,210 @@ function StatisticsSix() {
 			score: '- : -',
 			scoreAccent: false,
 			nameTwo: 'Melbourne Uni Bball',
+		},
+	]
+
+    const arrRowSix = [
+		{
+			number: 1,
+            show: true,
+			playTournamentClass: 'statistics-two__table-one-cell--playoff',
+			directionClass: '',
+			name: 'Хьюм Сити',
+			game: 4,
+			win: 4,
+			loss: 0,
+			scored: 403,
+			conceded: 335,
+			ratio: 1.203,
+			pct: 1,
+			points: 12,
+			forms: [
+				{ form: 'win' },
+				{ form: 'win' },
+				{ form: 'win' },
+				{ form: 'win' },
+			],
+		},
+		{
+			number: 2,
+            show: true,
+			playTournamentClass: 'statistics-two__table-one-cell--qualification',
+			directionClass: '',
+			name: 'Wyndham Basketball',
+			game: 3,
+			win: 3,
+			loss: 0,
+			scored: 326,
+			conceded: 232,
+			ratio: 1.405,
+			pct: 1,
+			points: 9,
+			forms: [
+				{ form: 'win' },
+				{ form: 'win' },
+				{ form: 'win' },
+			],
+		},
+		{
+			number: 3,
+            show: false,
+			playTournamentClass: 'statistics-two__table-one-cell--qualification',
+			directionClass: '',
+			name: 'Camberwell Dragons',
+			game: 5,
+			win: 3,
+			loss: 2,
+			scored: 436,
+			conceded: 467,
+			ratio: 0.934,
+			pct: 0.6,
+			points: 9,
+			forms: [
+				{ form: 'win' },
+				{ form: 'loss' },
+				{ form: 'win' },
+				{ form: 'win' },
+				{ form: 'loss' },
+			],
+		},
+		{
+			number: 4,
+            show: false,
+			playTournamentClass: 'statistics-two__table-one-cell--qualification',
+			directionClass: 'statistics-two__table-one-cell--up',
+			name: 'Keysborought',
+			game: 4,
+			win: 2,
+			loss: 2,
+			scored: 359,
+			conceded: 338,
+			ratio: 1.062,
+			pct: 0.5,
+			points: 6,
+			forms: [
+				{ form: 'win' },
+				{ form: 'win' },
+				{ form: 'loss' },
+				{ form: 'loss' },
+			],
+		},
+		{
+			number: 5,
+            show: false,
+			playTournamentClass: 'statistics-two__table-one-cell--qualification',
+			directionClass: 'statistics-two__table-one-cell--up',
+			name: 'Sanbury Jest',
+			game: 2,
+			win: 1,
+			loss: 1,
+			scored: 170,
+			conceded: 170,
+			ratio: 1,
+			pct: 0.5,
+			points: 3,
+			forms: [
+				{ form: 'loss' },
+				{ form: 'win' },
+			],
+		},
+		{
+			number: 6,
+            show: false,
+			playTournamentClass: '',
+			directionClass: 'statistics-two__table-one-cell--down',
+			name: 'Блэкберн Викингс',
+			game: 3,
+			win: 1,
+			loss: 2,
+			scored: 201,
+			conceded: 238,
+			ratio: 0.845,
+			pct: 0.333,
+			points: 3,
+			forms: [
+				{ form: 'loss' },
+				{ form: 'win' },
+				{ form: 'loss' },
+			],
+		},
+		{
+			number: 6,
+            show: false,
+			playTournamentClass: '',
+			directionClass: 'statistics-two__table-one-cell--down',
+			name: 'Маккиннон Кугарс',
+			game: 3,
+			win: 1,
+			loss: 2,
+			scored: 201,
+			conceded: 238,
+			ratio: 0.845,
+			pct: 0.333,
+			points: 3,
+			forms: [
+				{ form: 'loss' },
+				{ form: 'win' },
+				{ form: 'loss' },
+			],
+		},
+		{
+			number: 8,
+            show: false,
+			playTournamentClass: '',
+			directionClass: '',
+			name: 'Western Port Steelers',
+			game: 4,
+			win: 1,
+			loss: 3,
+			scored: 319,
+			conceded: 379,
+			ratio: 0.842,
+			pct: 0.25,
+			points: 3,
+			forms: [
+				{ form: 'loss' },
+				{ form: 'win' },
+				{ form: 'loss' },
+				{ form: 'loss' },
+			],
+		},
+		{
+			number: 9,
+            show: false,
+			playTournamentClass: '',
+			directionClass: 'statistics-two__table-one-cell--down',
+			name: 'Bellarine',
+			game: 4,
+			win: 0,
+			loss: 4,
+			scored: 293,
+			conceded: 340,
+			ratio: 0.862,
+			pct: 0,
+			points: 0,
+			forms: [
+				{ form: 'loss' },
+				{ form: 'loss' },
+				{ form: 'loss' },
+				{ form: 'loss' },
+			],
+		},
+		{
+			number: 9,
+            show: false,
+			playTournamentClass: '',
+			directionClass: 'statistics-two__table-one-cell--down',
+			name: 'Мульбурн Тайгерс',
+			game: 0,
+			win: 0,
+			loss: 0,
+			scored: 0,
+			conceded: 0,
+			ratio: 0,
+			pct: 0,
+			points: 0,
+			forms: [],
 		},
 	]
 
@@ -643,6 +851,79 @@ function StatisticsSix() {
                             </div>
                         </div>
                     </div>
+                </div>
+                <div className="statistics-six__pos">
+                    <h2 className="statistics-six__name-accent">Позиция команды</h2>
+                </div>
+                <div className="statistics-two__two-table-columns-wrapper">
+                    <h2 className="statistics-six__name-accent">Биг V (Викториан Стейт Чемпионшип)</h2>
+                    <div className="statistics-two__table-one">
+                        <div className="statistics-two__table-one-header">
+                            <div className="statistics-two__table-one-header-cell">Поз.</div>
+                            <div className="statistics-two__table-one-header-cell">Команда</div>
+                            <div className="statistics-two__table-one-header-cell">И</div>
+                            <div className="statistics-two__table-one-header-cell">В</div>
+                            <div className="statistics-two__table-one-header-cell">П</div>
+                            <div className="statistics-two__table-one-header-cell">ГЗа/ГПр</div>
+                            <div className="statistics-two__table-one-header-cell">Разн.</div>
+                            <div className="statistics-two__table-one-header-cell">Соотношение корзин</div>
+                            <div className="statistics-two__table-one-header-cell">pct</div>
+                            <div className="statistics-two__table-one-header-cell">Очки</div>
+                            <div className="statistics-two__table-one-header-cell">Форма</div>
+                        </div>
+                        {arrRowSix.map((item, index) => (item.show || (!item.show && showTable)) ? (
+                            <div key={index} className="statistics-two__table-one-row">
+                                <div className={`statistics-two__table-one-cell statistics-two__table-one-cell--number ${item.playTournamentClass} ${item.directionClass}`}>{item.number}</div>
+                                <div className="statistics-two__table-one-cell">{item.name}</div>
+                                <div className="statistics-two__table-one-cell">{item.game}</div>
+                                <div className="statistics-two__table-one-cell">{item.win}</div>
+                                <div className="statistics-two__table-one-cell">{item.loss}</div>
+                                <div className="statistics-two__table-one-cell">{item.scored}-{item.conceded}</div>
+                                <div className="statistics-two__table-one-cell">{item.scored - item.conceded}</div>
+                                <div className="statistics-two__table-one-cell">{item.ratio}</div>
+                                <div className="statistics-two__table-one-cell">{item.pct}</div>
+                                <div className="statistics-two__table-one-cell">{item.points}</div>
+                                <div className="statistics-two__table-one-cell statistics-two__table-one-cell--forms">
+                                    { item.forms.length > 0
+                                        ? <div className="statistics-two__table-one-forms">
+                                            {item.forms.map((form, index) => (
+                                                <div key={`key-${index}`} className={`statistics-two__table-one-form statistics-two__table-one-form--${form.form}`}>
+                                                    { form.form === 'win'? 'В' : 'П' }
+                                                </div>
+                                            ))} 
+                                        </div>
+                                        : ''
+                                    }
+                                </div>
+                            </div>
+
+                        ) : null
+                    
+                        )}
+                    </div>
+                    <div className={clsx(
+                        'statistics-two__table-one-info',
+                        {'statistics-two__table-one-info--hidden': !showTable},
+                    )}>
+                        <p className="statistics-two__table-one-info-text">Если две (или более) команды имеют одинаковое количество очков, следующие факторы помогут выявить победителя:</p>
+                        <p className="statistics-two__table-one-info-text">1. Процент побед и поражений</p>
+                        <p className="statistics-two__table-one-info-text">2. Очные встречи</p>
+                        <ul className="statistics-two__table-one-info-list">
+                            <li className="statistics-two__table-one-info-item">Плей-офф</li>
+                            <li className="statistics-two__table-one-info-item">Квалификация, Плей-офф</li>
+                        </ul>
+                    </div>
+                    <div className="statistics-two__table-two-btn-wrapper">
+                        <button 
+                            className="statistics-two__table-two-btn"
+                            onClick={() => setShowTable((showTable) => !showTable)}
+                        >{ showTable
+                            ? 'Скрыть таблицу'
+                            : 'Показать всю таблицу'
+                        }</button>
+                    </div>
+                </div>
+                <div className="statistics-two__two-table-columns-wrapper">
                 </div>
             </div>
 		</div>
