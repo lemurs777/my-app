b2f050e8f54f1f511bf636c6cc7477b240a7d044
lemurs777 <lemurs.agency@gmail.com>
2023-06-09 18:43:57 +0300

    Update FAQ Page

--- src/pages/FAQPage.tsx
+++ src/pages/FAQPage.tsx
@@ -1,15 +1,23 @@
 import { 
-    FaqBigList,
+	FaqBigList,
 } from '../components/';
 
+
+
 function FAQPage(){
-   return (
-       <>
-           <main className="page page--faq-big-list scroll">
-               <FaqBigList />
-           </main>
-       </>	
-   );
+
+	const infoFaqList = {
+		activeIndex: -1,
+		activeCategory: "bet",
+	}
+	
+	return (
+		<>
+			<main className="page page--faq-big-list scroll">
+				<FaqBigList infoFaqList={ infoFaqList }/>
+			</main>
+		</>	
+	);
 };
 
 export default FAQPage;
